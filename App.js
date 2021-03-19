16719357c3c753c858aa05dcc2a2f9e0ccb88975
mobiledev88 <adumas606@gmail.com>
2021-03-20 03:57:09 +0800

    change App.js

--- App.js
+++ App.js
@@ -22,7 +22,7 @@ const App = props => {
   return (
     <Provider store={reduxStore}>
       <AppearanceProvider>
-        <StatusBar />
+        // <StatusBar />
         <AppNavigator screenProps={{ theme: colorScheme }} />
       </AppearanceProvider>
     </Provider>
